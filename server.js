9bb4f4622e1629cb7ecd42c02a877379c38c4307
moodyomar <lonak.secret@gmail.com>
2021-08-09 19:36:39 +0300

    connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
+// connect mongoDB
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}))
+
 app.get('/',(req,res) => res.send('API RUNNING'))
+
+// define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
 
 const port = process.env.PORT || '5000' ;
 
