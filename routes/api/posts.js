3e8035e91abcf56e4129e797b1078c08e4adf44f
moodyomar <lonak.secret@gmail.com>
2021-08-15 21:41:27 +0300

    add & remove comments routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -147,5 +147,70 @@ router.put("/like/:id",auth,async(req,res) => {
     
   })
   
+  
+  // @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post("/comment/:id", [auth, [
+  check('text', 'Text is required').notEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+    await post.save();
+    res.json(post.comments);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete Comment
+// @access  Private
+router.delete("/comment/:id/:comment_id",auth,async(req,res) => {
+  try{
+    const post = await Post.findById(req.params.id);
+
+    // pull out comment from post
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    // make sure comment exists
+    if(!comment){
+      return res.status(404).json({msg:"Comment does not exists"});
+    }
+
+    // check user if he is the comment owner
+    if(comment.user.toString() !== req.user.id){
+      return res.status(401).json({msg:"User not Authorized"})
+    }
+    //  Get the remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex,1);
+    await post.save();
+    res.json(post.comments);
+
+  } catch(err) {
+      console.error(err);
+      res.status(500).json(err);
+  }
+  
+})
+
 
 module.exports = router;
