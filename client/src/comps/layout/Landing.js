3dec72c11c35e6f3ffcd9a72bd24b47a400d5034
moodyomar <lonak.secret@gmail.com>
2021-08-17 22:40:46 +0300

    miinor changes + add seperate api link

--- client/src/comps/layout/Landing.js
+++ client/src/comps/layout/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
   Link
 } from "react-router-dom";
 
@@ -11,7 +10,7 @@ return(
   <section className="landing">
   <div className="dark-overlay">
     <div className="landing-inner">
-      <h1 className="x-large">Developer Connector</h1>
+      <h1 className="x-large">Developers Networking</h1>
       <p className="lead">
         Create a developer profile/portfolio, share posts and get help from
         other developers
