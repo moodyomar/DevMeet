250e87e2af82452265c7035438a68e01d2c7b8b4
moodyomar <lonak.secret@gmail.com>
2021-08-31 09:02:17 +0300

    prepare for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const { originAllow } = require('./middleware/originAllow');
+const path = require('path');
 
 const app = express();
 
@@ -24,7 +24,6 @@ originAllow = (app) => {
 };
 originAllow(app);
 
-app.get('/',(req,res) => res.send('API RUNNING'))
 
 // define routes
 app.use('/api/users',require('./routes/api/users'));
@@ -32,6 +31,15 @@ app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
 
-const port = process.env.PORT || '5000' ;
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('client/build'))
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000 ;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
