6099c0636f30c50f7c800ee179ff5bfb7ce3b28a
moodyomar <lonak.secret@gmail.com>
2021-08-31 09:37:09 +0300

    seperate routes to other comp , handle 404 error

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from './comps/layout/Landing';
 import Navbar from './comps/layout/Navbar';
-import Login from './comps/auth/Login';
-import Alert from './comps/layout/Alert';
-import Register from './comps/auth/Register';
-import Dashboard from './comps/dashboard/Dashboard';
-import ProfileForm from './comps/profile-forms/ProfileForm';
-import PrivateRoute from './comps/routing/PrivateRoute';
+import Routes from './comps/routing/Routes';
+
 // redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,12 +11,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
-import AddExperience from './comps/profile-forms/AddExperience';
-import AddEducation from './comps/profile-forms/AddEducation';
-import Profiles from './comps/profiles/Profiles';
-import Profile from './comps/profile/Profile';
-import Posts from './comps/posts/Posts';
-import Post from './comps/post/Post';
+
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -37,23 +28,10 @@ const App = () => {
       <Provider store={store}>
         <Router>
           <Navbar />
+          <Switch>
           <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute path="/dashboard" component={Dashboard} />
-              <PrivateRoute exact path="/create-profile" component={ProfileForm} />
-              <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-              <PrivateRoute exact path="/add-experience" component={AddExperience} />
-              <PrivateRoute exact path="/add-education" component={AddEducation} />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute exact path="/posts/:id" component={Post} />
-            </Switch>
-          </section>
+          <Route component={Routes}/>
+          </Switch>
         </Router>
       </Provider>
     </>
---
+++ client/src/comps/layout/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const NotFound = () => (
+
+<>
+<h1 className="x-large text-primary">
+  <i className="fas fa-exclamation-triangle"> Page Not Found
+  </i>
+</h1>
+<p className="large">Sorry, This page does not exists.</p>
+</>
+
+)
+
+
+export default NotFound
