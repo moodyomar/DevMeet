2c78297ada061bfb9c9296360f3fcaff32885b46
moodyomar <lonak.secret@gmail.com>
2021-11-23 20:54:43 +0200

    fix ? map bug

--- client/src/comps/dashboard/Experience.js
+++ client/src/comps/dashboard/Experience.js
@@ -6,7 +6,7 @@ import { deleteExperience } from '../../actions/profile';
 
 const Experience = ({experience}) => { 
 const dispatch = useDispatch()
-const experiences = experience?.map(exp => (
+const experiences = experience.map(exp => (
   <tr key={exp._id}>
     <td>{exp.company}</td>
     <td className="hide-sm">{exp.title}</td>
